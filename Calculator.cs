2f39c99253bd1c81e7d71b9b22d31dd84a338b8a
Amin Hejazi <50262349+hejazia@users.noreply.github.com>
2026-08-30 23:22:18 +0330

    Add two-number addition function

---
+++ Calculator.cs
@@ -0,0 +1,9 @@
+namespace N8N_HW1_2;
+
+public static class Calculator
+{
+    public static double Add(double firstNumber, double secondNumber)
+    {
+        return firstNumber + secondNumber;
+    }
+}
